f2f7c620c19699cf8eea2d19ea1d7e5e5c8bf54e
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-11-06 17:25:09 +0100

    feat: setup

---
+++ components/Layout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Head from 'next/head';
+import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
+import useStyles from '../utils/styles';
+
+const Layout = ({ children }) => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>Next Amazona</title>
+      </Head>
+      <AppBar position="static" className={classes.navBar}>
+        <Toolbar>
+          <Typography>amazona</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved. next Amazona</Typography>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import Layout from '../components/Layout';
+
+import data from '../utils/data';
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core';
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => {
+            return (
+              <Grid item md={4} key={product.name}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Typography>{'$' + product.price}</Typography>
+                    <Button size="small" color="primary">
+                      Add to Cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </div>
+    </Layout>
+  );
+}
