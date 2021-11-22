b5fd74fcfd7101171f39b98ea8c86c977f12dd58
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-11-22 23:31:36 +0100

    feat: getting order info from server

---
+++ pages/api/orders/history.js
@@ -0,0 +1,16 @@
+import nextConnect from 'next-connect';
+import Order from '../../../models/Order';
+import { isAuth } from '../../../utils/auth';
+import db from '../../../utils/db';
+import { onError } from '../../../utils/db';
+
+const handler = nextConnect({ onError });
+handler.use(isAuth);
+
+handler.get(async (req, res) => {
+  await db.connect();
+  const orders = await Order.find({ user: req.user._id });
+  res.send(orders);
+});
+
+export default handler;
