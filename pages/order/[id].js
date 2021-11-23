dc4b3d8c278a15d1be0b8f08976316e5c678ed8a
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-11-23 16:30:27 +0100

    feat: added deliver button to deliver paid orders

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -12,6 +12,7 @@ import {
   List,
   ListItem,
   CircularProgress,
+  Button,
 } from '@material-ui/core';
 import Layout from '../../components/Layout';
 import { Store } from '../../utils/Store';
@@ -42,6 +43,19 @@ function reducer(state, action) {
       return { ...state, loadingPay: false, errorPay: action.payload };
     case 'PAY_RESET':
       return { ...state, loadingPay: false, successPay: false, errorPay: '' };
+    case 'DELIVER_REQUEST':
+      return { ...state, loadingDeliver: true };
+    case 'DELIVER_SUCCESS':
+      return { ...state, loadingDeliver: false, successDeliver: true };
+    case 'DELIVER_FAIL':
+      return { ...state, loadingDeliver: false, errorDeliver: action.payload };
+    case 'DELIVER_RESET':
+      return {
+        ...state,
+        loadingDeliver: false,
+        successDeliver: false,
+        errorDeliver: '',
+      };
     default:
       state;
   }
@@ -55,14 +69,14 @@ const Order = ({ params }) => {
   const classes = useStyles();
   const { userInfo } = state;
 
-  const [{ loading, error, order, successPay }, dispatch] = useReducer(
-    reducer,
-    {
-      loading: true,
-      order: {},
-      error: '',
-    }
-  );
+  const [
+    { loading, error, order, successPay, loadingDeliver, successDeliver },
+    dispatch,
+  ] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    error: '',
+  });
 
   const {
     shippingAddress,
@@ -94,10 +108,18 @@ const Order = ({ params }) => {
         dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
-    if (!order._id || successPay || (order._id && order._id !== orderId)) {
+    if (
+      !order._id ||
+      successPay ||
+      successDeliver ||
+      (order._id && order._id !== orderId)
+    ) {
       fetchOrder();
       if (successPay) {
         dispatch({ type: 'PAY_RESET' });
+      }
+      if (successDeliver) {
+        dispatch({ type: 'DELIVER_RESET' });
       }
     } else {
       const loadPaypalScript = async () => {
@@ -112,7 +134,7 @@ const Order = ({ params }) => {
       };
       loadPaypalScript();
     }
-  }, [order, successPay]);
+  }, [order, successPay, successDeliver]);
 
   const { closeSnackbar, enqueueSnackbar } = useSnackbar();
 
@@ -153,6 +175,24 @@ const Order = ({ params }) => {
   function onError(err) {
     enqueueSnackbar(getError(err), { variant: 'error' });
   }
+
+  const deliverOrderHandler = async () => {
+    try {
+      dispatch({ type: 'DELIVER_REQUEST' });
+      const { data } = await axios.put(
+        `/api/orders/${order._id}/deliver`,
+        {},
+        {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({ type: 'DELIVER_SUCCESS', payload: data });
+      enqueueSnackbar('Order is delivered', { variant: 'success' });
+    } catch (err) {
+      dispatch({ type: 'DELIVER_FAIL', payload: getError(err) });
+      enqueueSnackbar(getError(err), { variant: 'error' });
+    }
+  };
   return (
     <Layout title={`Order ${orderId}`}>
       <Typography component="h1" variant="h1">
@@ -327,6 +367,19 @@ const Order = ({ params }) => {
                         />
                       </div>
                     )}
+                  </ListItem>
+                )}
+                {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+                  <ListItem>
+                    {loadingDeliver && <CircularProgress />}
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      onClick={deliverOrderHandler}
+                    >
+                      Deliver Order
+                    </Button>
                   </ListItem>
                 )}
               </List>
