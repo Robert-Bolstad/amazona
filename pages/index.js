b70d61d073e5d537dfd4b33a135cb88906a9c2d3
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-11-27 20:58:24 +0100

    feat: added stars for rating

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,7 @@ import axios from 'axios';
 import { Store } from '../utils/Store';
 import { useRouter } from 'next/router';
 import { useContext } from 'react';
+import { Rating } from '@material-ui/lab';
 
 export default function Home({ products }) {
   const router = useRouter();
@@ -35,51 +36,50 @@ export default function Home({ products }) {
     router.push('/cart');
   };
   return (
-    <Layout>
-      <div>
-        <h1>Products</h1>
-        <Grid container spacing={3}>
-          {products.map((product) => {
-            return (
-              <Grid item md={4} key={product.name}>
-                <Card>
-                  <NextLink href={`/product/${product.slug}`} passHref>
-                    <Link>
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          image={product.image}
-                          title={product.name}
-                        ></CardMedia>
-                        <CardContent>
-                          <Typography>{product.name}</Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Link>
-                  </NextLink>
-                  <CardActions>
-                    <Typography>{'$' + product.price}</Typography>
-                    <Button
-                      size="small"
-                      color="primary"
-                      onClick={() => addToCartHandler(product)}
-                    >
-                      Add to Cart
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            );
-          })}
-        </Grid>
-      </div>
+    <Layout title="Home">
+      <h1>Products</h1>
+      <Grid container spacing={3}>
+        {products.map((product) => {
+          return (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <Link>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        image={product.image}
+                        title={product.name}
+                      ></CardMedia>
+                      <CardContent>
+                        <Typography>{product.name}</Typography>
+                        <Rating value={product.rating} readOnly></Rating>
+                      </CardContent>
+                    </CardActionArea>
+                  </Link>
+                </NextLink>
+                <CardActions>
+                  <Typography>{'$' + product.price}</Typography>
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => addToCartHandler(product)}
+                  >
+                    Add to Cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
     </Layout>
   );
 }
 
 export async function getServerSideProps() {
   await db.connect();
-  const products = await Product.find({}).lean();
+  const products = await Product.find({}, '-reviews').lean();
   await db.disconnect();
   return {
     props: {
